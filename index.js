a1538549d612ca8000c87b7caa249bc42d92d326
clarks <leiferemil@gmail.com>
2023-11-22 18:40:17 +0100

    bases of game added

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 import Game from "./src/game.js";
+import View from "./src/view.js";
+
+const root = document.querySelector("#root");
 
 const game = new Game();
+const view = new View(root, 320, 640, 20, 10);
 
-window.game=game;
+window.game = game;
+window.view = view;
 
-console.log(game)
+document.addEventListener("keydown", (evend) => {
+  switch (event.keyCode) {
+    case 37: //LEFT ARROW
+      game.movePieceLeft();
+      view.render(game.getState());
+      break;
+    case 38: //UP ARROW
+      game.rotatePiece();
+      view.render(game.getState());
+      break;
+    case 39: //RIGHT ARROW
+      game.movePieceRight();
+      view.render(game.getState());
+      break;
+    case 40: //DOWN ARROW
+      game.movePieceDown();
+      view.render(game.getState());
+      break;
+  }
+});
+
