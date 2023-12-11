b4e863a2bc393b4d99b8e3e1b53347d696f57da6
clarks <leiferemil@gmail.com>
2023-12-11 15:08:01 +0100

    Update server.js

--- backendTetris/server.js
+++ backendTetris/server.js
@@ -10,68 +10,6 @@ const io = socketIO(server);
 const PORT = process.env.PORT || 3000;
 const SCORES_FILE_PATH = 'data/scores.json';
 
-let userScores;
-
-app.use(express.static("FrontEnd"));
-
-function loadScores() {
-  try {
-    const data = fs.readFileSync(SCORES_FILE_PATH, 'utf8');
-    return JSON.parse(data) || userScores || {};
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      saveScores({});
-      return userScores || {};
-    } else {
-      console.error(`Ошибка при загрузке счетов: ${error.message}`);
-      return userScores || {};
-    }
-  }
-}
-
-function saveScores() {
-  try {
-    if (userScores) {
-      fs.writeFileSync(SCORES_FILE_PATH, JSON.stringify(userScores, null, 2), 'utf8');
-      console.log('Счета сохранены в файл');
-    }
-  } catch (error) {
-    console.error(`Ошибка при сохранении счетов: ${error.message}`);
-  }
-}
-
-userScores = loadScores();
-
-app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-  console.log('Пользователь подключился');
-
-  socket.on('disconnect', () => {
-    console.log('Пользователь отключился');
-  });
-
-  socket.on('updateScore', (score) => {
-    const userId = socket.id;
-
-    if (!userScores) {
-      userScores = loadScores();
-    }
-
-    userScores[userId] = score;
-
-    saveScores();
-
-    console.log(`Счет пользователя ${userId} обновлен: ${score}`);
-  });
-
-});
-
-app.get('/scores', (req, res) => {
-  const scores = loadScores();
-  res.json(scores);
-});
-
 app.get("/tetrus", function(req, res){
         
   res.sendFile(__dirname + '/FrontEnd/index.html')
